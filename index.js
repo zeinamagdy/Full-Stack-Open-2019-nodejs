61ba6bdceae3668711f02f7f59b0eacb80bc64dd
zeinamagdy <zainabmagdy8@gmail.com>
2019-11-01 16:03:37 +0100

    solving error:Failed to bind to $PORT

--- index.js
+++ index.js
@@ -84,5 +84,5 @@ app.post('/api/persons', (req, res) => {
 })
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    // console.log(`Server running on port ${PORT}`)
 })
