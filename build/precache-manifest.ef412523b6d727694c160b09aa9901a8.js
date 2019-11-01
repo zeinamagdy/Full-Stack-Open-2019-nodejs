e06e07239f6e9e24367bf3d0d8b0d8483f0f9575
zeinamagdy <zainabmagdy8@gmail.com>
2019-11-01 15:07:44 +0100

    build frontend

---
+++ build/precache-manifest.ef412523b6d727694c160b09aa9901a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd1706e7d5fceefa437983275570ff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682bf7aa6ff52ab20fdd",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4f66a24de54efbf3368f",
+    "url": "/static/js/2.32e8b734.chunk.js"
+  },
+  {
+    "revision": "682bf7aa6ff52ab20fdd",
+    "url": "/static/js/main.b349543b.chunk.js"
+  },
+  {
+    "revision": "3102baec9d8f0d6c2c2c",
+    "url": "/static/js/runtime-main.6f9779a4.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const cors = require('cors')
-
 morgan.token('body', function getbody(req) {
     return JSON.stringify(req.body)
 })
-
 const app = express()
-
-app.use(cors())
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :response-time :body'))
 
@@ -86,7 +82,7 @@ app.post('/api/persons', (req, res) => {
     persons = persons.concat(person)
     res.json(person)
 })
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
